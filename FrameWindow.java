5bc13d76cde47834013edb6711b140a66ddcbf6c
Porom <porom.kamal@gmail.com>
2021-08-21 18:18:22 -0400

    Try

--- FrameWindow.java
+++ FrameWindow.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
 
-class Frame{
+class FrameWindow{
     private int width;
     private int height;
     private JFrame main_frame;
-    public Frame(int sizex, int sizey){
+    public FrameWindow(int sizex, int sizey){
         this.width = sizex;
         this.height = sizey;
     }
