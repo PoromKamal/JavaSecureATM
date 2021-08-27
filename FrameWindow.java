d819e24537c8133ed0ba05d1015e4cb616176707
Porom <porom.kamal@gmail.com>
2021-08-26 21:14:24 -0400

    Started on signup page.

--- FrameWindow.java
+++ FrameWindow.java
@@ -1,16 +1,17 @@
 import java.awt.*;
-
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
-import javax.swing.text.StyleConstants.FontConstants;
 
-class FrameWindow{
+class FrameWindow implements ActionListener{
     private int width;
     private int height;
     private JFrame main_frame;
+    private Color MAIN_COLOR = Color.DARK_GRAY;
+    private String MAIN_FONT = "Arial";
     public FrameWindow(int sizex, int sizey){
         this.width = sizex;
         this.height = sizey;
@@ -30,7 +31,7 @@ class FrameWindow{
         logoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         //Get motto
         JLabel motto = new JLabel();
-        motto.setFont(new Font("Arial", Font.BOLD, 20));
+        motto.setFont(new Font(MAIN_FONT, Font.BOLD, 20));
         motto.setForeground(Color.WHITE);
         motto.setText("Confidence Through Security");
         motto.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -52,7 +53,7 @@ class FrameWindow{
         login.setMaximumSize(new Dimension(750, 300));
         login.setAlignmentX(Component.CENTER_ALIGNMENT);
         login.setLayout(new BoxLayout(login, BoxLayout.Y_AXIS));
-        login.setBackground(Color.DARK_GRAY);
+        login.setBackground(MAIN_COLOR);
         login.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.WHITE), "Sign-in", TitledBorder.CENTER, TitledBorder.TOP,new Font("Arial", Font.BOLD, 18), Color.WHITE));
         Border border = login.getBorder();
         Border margin = new EmptyBorder(30, 30, 30, 30);
@@ -62,9 +63,9 @@ class FrameWindow{
         JPanel userName = new JPanel();
         userName.setBorder(new EmptyBorder(10,10,10,10));
         userName.setMaximumSize(new Dimension(800, 30));
-        userName.setBackground(Color.DARK_GRAY);
+        userName.setBackground(MAIN_COLOR);
         JLabel userNameText = new JLabel();
-        userNameText.setFont(new Font("Arial", Font.BOLD, 15));
+        userNameText.setFont(new Font(MAIN_FONT, Font.BOLD, 15));
         userNameText.setForeground(Color.WHITE);
         userNameText.setText("Username: ");
         JTextField userNameField = new JTextField(15);
@@ -75,10 +76,10 @@ class FrameWindow{
         //Set password panel
         JPanel password = new JPanel();
         password.setBorder(new EmptyBorder(10,10,10,10));
-        password.setBackground(Color.DARK_GRAY);
+        password.setBackground(MAIN_COLOR);
         password.setMaximumSize(new Dimension(800, 40));
         JLabel passwordText = new JLabel();
-        passwordText.setFont(new Font("Arial", Font.BOLD, 15));
+        passwordText.setFont(new Font(MAIN_FONT, Font.BOLD, 15));
         passwordText.setForeground(Color.WHITE);
         passwordText.setText("Password: ");
         JPasswordField passwordField = new JPasswordField(15);
@@ -87,7 +88,7 @@ class FrameWindow{
         
         //Add login button
         JPanel signinButton = new JPanel();
-        signinButton.setBackground(Color.DARK_GRAY);
+        signinButton.setBackground(MAIN_COLOR);
         signinButton.setBorder(new EmptyBorder(10, 10, 10, 10));
         JButton signin = new JButton("Login");
         signin.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -104,13 +105,16 @@ class FrameWindow{
         Border signupBorder = signup.getBorder();
         Border signupMargin = new EmptyBorder(30, 30, 30, 30);
         signup.setBorder(new CompoundBorder(signupMargin, signupBorder));
-        signup.setBackground(Color.DARK_GRAY);
+        signup.setBackground(MAIN_COLOR);
 
         JLabel signupText = new JLabel();
         signupText.setFont(new Font("Times New Roman", Font.BOLD, 17));
         signupText.setForeground(Color.WHITE);
         signupText.setText("Don't have an account? Sign-up here: ");
         JButton signupButton = new JButton("Sign-up");
+        
+        
+        
         signup.add(signupText);
         signup.add(signupButton);
 
@@ -120,9 +124,88 @@ class FrameWindow{
         main_frame.getContentPane().add(login);
         main_frame.getContentPane().add(signup);
         //set background color
-        main_frame.getContentPane().setBackground(Color.DARK_GRAY);
+        main_frame.getContentPane().setBackground(MAIN_COLOR);
         main_frame.pack();
+        JPanel main_content = (JPanel)main_frame.getContentPane();
+        //Button actionlistener
+        signupButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                signupPage(main_content);
+            }});
+
         main_frame.setVisible(true);
-        
     }
+
+    public void signupPage(JPanel main_content){
+        /*
+        Signup page will contain the following data fields:
+            - First Name
+            - Last Name
+            - Date of Birth (Drop down menu)
+            - Username
+            - Email
+            - Password
+            - confirm password
+        */
+        JPanel signupPage = new JPanel();
+        signupPage.setLayout(null);
+        signupPage.setBackground(MAIN_COLOR);
+        signupPage.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.WHITE), "Sign Up", TitledBorder.CENTER, TitledBorder.TOP,new Font("Arial", Font.BOLD, 20), Color.WHITE));
+
+        //Buttons (back, and signup)
+        JPanel buttons = new JPanel();
+        buttons.setBackground(Color.WHITE);
+        buttons.setAlignmentX(Component.LEFT_ALIGNMENT);
+        buttons.setMaximumSize(new Dimension(40, 40));
+        JButton backButton = new JButton("back");
+        JButton signupButton = new JButton("Sign-up");
+        backButton.setBounds(0,0,80,30); 
+        buttons.add(backButton);
+        buttons.add(signupButton);
+        
+        //First name field
+        JPanel firstNamePanel = new JPanel();
+        firstNamePanel.setBackground(MAIN_COLOR);
+        firstNamePanel.setMaximumSize(new Dimension(700, 80));
+        JLabel firstNameText = new JLabel();
+        firstNameText.setFont(new Font(MAIN_FONT, Font.PLAIN, 13));
+        firstNameText.setForeground(Color.WHITE);
+        firstNameText.setText("First Name: ");
+        JTextField firstNameInput = new JTextField(15);
+        firstNamePanel.add(firstNameText);
+        firstNamePanel.add(firstNameInput);
+
+        //Last name field
+        JPanel lastNamePanel = new JPanel();
+        lastNamePanel.setBackground(MAIN_COLOR);
+        lastNamePanel.setMaximumSize(new Dimension(700, 80));
+        JLabel lastNameText = new JLabel();
+        lastNameText.setFont(new Font(MAIN_FONT, Font.PLAIN, 13));
+        lastNameText.setForeground(Color.WHITE);
+        lastNameText.setText("Last Name: ");
+        JTextField lastNameInput = new JTextField(15);
+        lastNamePanel.add(lastNameText);
+        lastNamePanel.add(lastNameInput);
+
+        //Add in all the fields to the signup page panel
+        signupPage.setLayout(new BoxLayout(signupPage, BoxLayout.Y_AXIS));
+        signupPage.add(firstNamePanel);
+        signupPage.add(lastNamePanel);
+        signupPage.add(backButton);
+
+
+        //Set the main_frame content
+        main_frame.setContentPane(signupPage);
+        main_frame.validate();
+        backButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                main_frame.setContentPane(main_content);
+                main_frame.validate();
+            }});
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){}
 }
